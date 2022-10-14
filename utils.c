fd465607745e36ff872aef7cc6b611aa89ec2c5d
Swanav <swanav@outlook.com>
2022-10-14 01:55:41 -0700

    Small changes Backend servers almost ready.

--- utils.c
+++ utils.c
@@ -110,3 +110,107 @@ void udp_server_send(udp_server_t* server, udp_endpoint_t* dst, udp_dgram_t* dgr
         }
     }
 }
+
+db_entry_t* read_courses_db(char* filename) {
+    db_entry_t* head = NULL;
+    db_entry_t* tail = NULL;
+    FILE* fp = csv_open(filename);
+    char line[1024];
+
+    if (fp == NULL) {
+        return NULL;
+    }
+
+    while (fgets(line, sizeof(line), fp)) {
+        char* token = strtok(line, ",");
+        if (token == NULL) {
+            continue;
+        }
+        db_entry_t* entry = (db_entry_t*)malloc(sizeof(db_entry_t));
+        if (entry == NULL) {
+            LOGEM("Failed to allocate memory for db_entry_t");
+            continue;
+        }
+        memset(entry, 0, sizeof(db_entry_t));
+        strncpy(entry->course_code, token, sizeof(entry->course_code));
+        token = strtok(NULL, ",");
+        if (token == NULL) {
+            continue;
+        }
+        entry->credits = atoi(token);
+        token = strtok(NULL, ",");
+        if (token == NULL) {
+            continue;
+        }
+        strncpy(entry->professor, token, sizeof(entry->professor));
+        token = strtok(NULL, ",");
+        if (token == NULL) {
+            continue;
+        }
+        strncpy(entry->days, token, sizeof(entry->days));
+        token = strtok(NULL, ",");
+        if (token == NULL) {
+            continue;
+        }
+        strncpy(entry->course_name, token, sizeof(entry->course_name));
+        if (head == NULL) {
+            head = entry;
+            tail = entry;
+        } else {
+            tail->next = entry;
+            tail = entry;
+        }
+    }
+
+    fclose(fp);
+    return head;
+}
+
+void free_courses_db(db_entry_t* head) {
+    db_entry_t* entry = head;
+    while (entry != NULL) {
+        db_entry_t* next = entry->next;
+        free(entry);
+        entry = next;
+    }
+}
+
+int parse_lookup_request(udp_dgram_t* req_dgram, lookup_params_t* params) {
+    char* token = strtok(req_dgram->data, ",");
+    if (token == NULL) {
+        return -1;
+    }
+    strncpy(params->course_code, token, sizeof(params->course_code));
+
+    token = strtok(NULL, ",");
+    if (token == NULL) {
+        return -1;
+    }
+
+    strncpy(params->category, token, sizeof(params->category));
+
+    return 0;
+}
+
+lookup_result_t course_lookup(const db_entry_t* db, char* category, char* course_code, char* info_buf, size_t info_buf_len) {
+    db_entry_t* ptr = (db_entry_t*) db;
+    while(ptr) {
+        if (strcmp(ptr->course_code, course_code) == 0) {
+            if (strcmp(category, "Credits") == 0) {
+                snprintf(info_buf, info_buf_len, "%d", ptr->credits);
+                return LOOKUP_OK;
+            } else if (strcmp(category, "Professor") == 0) {
+                strncpy(info_buf, ptr->professor, info_buf_len);
+                return LOOKUP_OK;
+            } else if (strcmp(category, "Days") == 0) {
+                strncpy(info_buf, ptr->days, info_buf_len);
+                return LOOKUP_OK;
+            } else if (strcmp(category, "CourseName") == 0) {
+                strncpy(info_buf, ptr->course_name, info_buf_len);
+                return LOOKUP_OK;
+            }
+        }
+        ptr =  ptr->next;
+    }
+    return LOOKUP_FAIL;
+}
--- utils.h
+++ utils.h
@@ -57,4 +57,28 @@ void udp_server_stop(udp_server_t* server);
 void udp_server_receive(udp_server_t* server);
 void udp_server_send(udp_server_t* server, udp_endpoint_t* dest, udp_dgram_t* datagram);
 
+typedef struct __lookup_params_t {
+    char category[64];
+    char course_code[64];
+} lookup_params_t;
+
+typedef enum __lookup_result_t {
+    LOOKUP_OK,
+    LOOKUP_FAIL,
+} lookup_result_t;
+
+typedef struct __db_entry_t {
+    char course_code[32];
+    int credits;
+    char professor[64];
+    char days[32];
+    char course_name[128];
+    struct __db_entry_t* next;
+} db_entry_t;
+
+db_entry_t* read_courses_db(char* filename);
+void free_courses_db(db_entry_t* head);
+int parse_lookup_request(udp_dgram_t* req_dgram, lookup_params_t* params);
+lookup_result_t course_lookup(const db_entry_t* db, char* category, char* course_code, char* info_buf, size_t info_buf_len);
+
 #endif // __UTILS_H__
